13aaea074e68e1e23c681ecf1b83fd741128cd80
AnweshDahal <anweshd2000@gmail.com>
2020-12-31 16:58:07 +0545

    Adds Team Info and Comments

--- BinarySearch.java
+++ BinarySearch.java
@@ -12,19 +12,31 @@ package wosaa_fashion_store_is;
 public class BinarySearch {
     public static int search(Object[][] array, int low, int high, int find){
         /**
-         * Binary Search to find the index of the price in 
+         * Binary Search to find the index of the price in the sorted list
          */
+        
         if (low <= high){
-            int mid = (low + high) / 2;
+            int mid = (low + high) / 2; // Gets the midpoint of the Array
             if (Integer.parseInt((String)array[mid][6]) == find){
+                // if the item at midpoint is the required item, 
+                // return the index
                 return mid;
             } else if (Integer.parseInt((String)array[mid][6]) > find){
+                // if the item at midpoint is larger than the item required
+                // assigns the value of the midpoint + 1 to high
                 high = mid - 1;
             } else if (Integer.parseInt((String)array[mid][6]) < find){
+                // if the item at midpoint is lower than the required item
+                // assign the value of midpoint -1 to low
                 low = mid + 1;
             }
+            // loops recursively until either
+            // item at midpoint equals the required item
+            // or low >= high
             return search(array, low, high, find);
         } else {
+            // if low >= high returns -1
+            // indicating item not found
             return -1;
         }
     }
--- MergeSorter.java
+++ MergeSorter.java
@@ -10,34 +10,56 @@ package wosaa_fashion_store_is;
  * @author 19031710 Srijan Adhikari
  */
 public class MergeSorter {
+    /**
+     * Sorts the array using merge sort
+     * 
+     * @param mainArray the array that needs to be sorted
+     */
     public static void sort(Object[][] mainArray){
         if (mainArray.length <= 1){
-            // if the length of the main array is less than one then the method is terminated from here
+            // if the length of the main array is less than one then the method 
+            // is terminated from here
             return;
         }
         
+        // The first array and the second Array 
+        // is half the size of original array
         Object[][] firstArray = new Object[mainArray.length / 2][7];
         Object[][] secondArray = new Object[mainArray.length - firstArray.length][7];
         
+        // Inserting value in first array
         for (int i = 0; i < firstArray.length; i++){
             firstArray[i] = mainArray[i];
         }
         
+        // Inserting value in second array
         for (int i = 0; i < secondArray.length; i++){
             secondArray[i] = mainArray[firstArray.length + i];
         }
         
+        // Breaks the arrays until their length is 1
         sort(firstArray);
         sort(secondArray);
         
+        // merges the firstArray and secondArray into mainArray
         merge(firstArray, secondArray, mainArray);
     }
     
+    /**
+     * Sorts the main array by merging the firstArray and the secondArray
+     * 
+     * @param firstArray the array containing 1st 1/2 of main array
+     * @param secondArray the array containing 2nd 1/2 of main array
+     * @param mainArray the target array that needs to be sorted
+     */
     private static void merge(Object[][] firstArray, Object[][] secondArray, Object[][] mainArray){
         int indexFirstArray = 0;
         int indexSecondArray = 0;
         int indexMainArray = 0;
         
+        // Compares the elements of the first/ second row and
+        // sorts them in the main array until the End of Index is reached
+        // in any one of the array
         while (indexFirstArray < firstArray.length && indexSecondArray < secondArray.length){
             if(Integer.parseInt((String)firstArray[indexFirstArray][6]) < Integer.parseInt((String)secondArray[indexSecondArray][6])){
                 mainArray[indexMainArray] = firstArray[indexFirstArray];
